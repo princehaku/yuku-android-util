b13e8b97597d59a9c96d21445172e7e9a3e838c0
mimpi <mimpi@eskimos>
2010-04-06 21:14:56 +0000

    baca perikop dan keluarin Blok dengan betul

--- BintexReader/src/yuku/bintex/BintexReader.java
+++ BintexReader/src/yuku/bintex/BintexReader.java
@@ -4,6 +4,8 @@ import java.io.*;
 
 public class BintexReader {
 	private final InputStream is_;
+	
+	private int pos_ = 0;
 
 	public BintexReader(InputStream is) {
 		this.is_ = is;
@@ -11,6 +13,7 @@ public class BintexReader {
 	
 	public String readShortString() throws IOException {
 		int len = is_.read();
+		pos_++;
 		
 		if (len < 0) {
 			throw new EOFException();
@@ -38,19 +41,31 @@ public class BintexReader {
 	}
 	
 	public int readInt() throws IOException {
-		return (is_.read() << 24) | (is_.read() << 16) | (is_.read() << 8) | (is_.read());
+		int res = (is_.read() << 24) | (is_.read() << 16) | (is_.read() << 8) | (is_.read());
+		pos_ += 4;
+		return res;
 	}
 
 	public char readChar() throws IOException {
-		return (char) ((is_.read() << 8) | (is_.read()));
+		char res = (char) ((is_.read() << 8) | (is_.read()));
+		pos_ += 2;
+		return res;
 	}
 
 	public int readUint8() throws IOException {
-		return is_.read();
+		int res = is_.read();
+		pos_++;
+		return res;
 	}
 	
 	public long skip(long n) throws IOException {
-		return is_.skip(n);
+		long res = is_.skip(n);
+		pos_ += (int) res;
+		return res;
+	}
+	
+	public int getPos() {
+		return pos_;
 	}
 	
 	public void close() {
