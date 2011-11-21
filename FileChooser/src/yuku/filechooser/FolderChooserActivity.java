c9f2c67d95ff392c896cecd12976398ed46f2dc3
Yuku <yuku@ikitek.com>
2011-11-21 19:17:58 +0800

    ADd new folder func to FolderChooser

--- FileChooser/src/yuku/filechooser/FolderChooserActivity.java
+++ FileChooser/src/yuku/filechooser/FolderChooserActivity.java
@@ -2,14 +2,17 @@ package yuku.filechooser;
 
 import android.app.*;
 import android.content.*;
+import android.content.pm.*;
 import android.graphics.*;
 import android.os.*;
 import android.text.*;
 import android.text.style.*;
 import android.view.*;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.TextView.BufferType;
 
 import java.io.*;
@@ -40,7 +43,7 @@ public class FolderChooserActivity extends Activity {
 	FolderChooserConfig config;
 	TreeAdapter adapter;
 	File selectedDir;
-
+	
 	@Override public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		config = getIntent().getParcelableExtra(EXTRA_config);
@@ -61,6 +64,11 @@ public class FolderChooserActivity extends Activity {
 		tree.setAdapter(adapter);
 		tree.setOnItemClickListener(tree_itemClick);
 		
+		perm_writeExt = checkCallingOrSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+		if (perm_writeExt == PackageManager.PERMISSION_GRANTED) {
+			tree.setOnItemLongClickListener(tree_itemLongClick);
+		}
+		
 		bOk.setOnClickListener(bOk_click);
 
 		setSelectedDir(null);
@@ -68,7 +76,6 @@ public class FolderChooserActivity extends Activity {
 		if (config.roots == null || config.roots.size() == 0) {
 			config.roots = Arrays.asList(Environment.getExternalStorageDirectory().getAbsolutePath());
 		}
-		
 		
 		File[] children = new File[config.roots.size()];
 		for (int i = 0; i < config.roots.size(); i++) {
@@ -91,6 +98,58 @@ public class FolderChooserActivity extends Activity {
 		}
 	};
 	
+	private OnItemLongClickListener tree_itemLongClick = new OnItemLongClickListener() {
+		@Override public boolean onItemLongClick(AdapterView<?> parent, View v, final int position, long id) {
+			final FileTreeNode node = (FileTreeNode) adapter.getItem(position);
+			final File file = node.getFile();
+			if (!file.isDirectory()) return false;
+			
+			new AlertDialog.Builder(FolderChooserActivity.this)
+			.setItems(new String[] {"New folder"}, new DialogInterface.OnClickListener() {
+				@Override public void onClick(DialogInterface _unused_, int which) {
+					final Button[] bOk = {null};
+					
+					final EditText tFolderName = new EditText(FolderChooserActivity.this);
+					MarginLayoutParams lp = new MarginLayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+					lp.leftMargin = lp.topMargin = lp.rightMargin = lp.bottomMargin = (int) (6 * getResources().getDisplayMetrics().density);
+					tFolderName.setLayoutParams(lp);
+					tFolderName.setHint("Folder name");
+					tFolderName.addTextChangedListener(new TextWatcher() {
+						@Override public void afterTextChanged(Editable s) {
+							bOk[0].setEnabled(s.toString().trim().length() > 0);
+						}
+
+						@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+						@Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+					});
+					
+					final AlertDialog dialog = new AlertDialog.Builder(FolderChooserActivity.this)
+					.setTitle("New folder")
+					.setView(tFolderName)
+					.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+						@Override public void onClick(DialogInterface dialog, int which) {
+							String folderName = tFolderName.getText().toString().trim();
+							File newDir = new File(file, folderName);
+							boolean ok = newDir.mkdirs();
+							if (ok) {
+								if (node.getExpanded()) {
+									node.setExpanded(false);
+								}
+								node.setExpanded(true);
+							}
+						}})
+					.show();
+					
+					bOk[0] = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+					bOk[0].setEnabled(false);
+				}
+			})
+			.setNegativeButton(android.R.string.cancel, null)
+			.show();
+			return true;
+		}
+	};
+		
 	OnClickListener bOk_click = new OnClickListener() {
 		@Override public void onClick(View v) {
 			if (selectedDir == null) return;
@@ -105,6 +164,7 @@ public class FolderChooserActivity extends Activity {
 			finish();
 		}
 	};
+	private int perm_writeExt;
 
 	class FileTreeNode extends BaseFileTreeNode {
 		private String label;
