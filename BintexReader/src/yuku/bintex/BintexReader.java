80113434b767122f45b1e7d362f08c13b234b94e
Yuku on yuku5 <yukuku@gmail.com>
2012-12-12 15:39:18 +0700

    Bintex: Support for value types. Value is a typed, more compact representation for small ints (-1..7), small strings (len < 16 and null), and also supports int array and simple map. Simple map is a map with no more than 255 entries and whose keys are 8-bit strings with max len 255.

--- BintexReader/src/yuku/bintex/BintexReader.java
+++ BintexReader/src/yuku/bintex/BintexReader.java
@@ -3,12 +3,35 @@ package yuku.bintex;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class BintexReader {
+	private static final int[] SUPPORTED_TYPE_MAP = { // 1 = int; 2 = string; 3 = int[]; 4 = simple map
+		//.1 .2 .3 .4 .5 .6 .7 .8 .9 .a .b .c .d .e .f
+		0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 1, 1, // 0. 
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 1. 
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 2. 
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 3. 
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 4. 
+		0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // 5. 
+		0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // 6. 
+		2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 7. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 8. 
+		4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 9. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // a. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // b. 
+		3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, // c. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // d. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // e. 
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // f. 
+	};
+	
 	private final InputStream is_;
 	
 	private int pos_ = 0;
-	private char[] buf = new char[1024]; // paling dikit 255 biar bisa shortstring
+	private byte[] buf_byte = new byte[2048]; 
+	private char[] buf_char = new char[1024]; 
 	
 	public BintexReader(InputStream is) {
 		this.is_ = is;
@@ -25,9 +48,9 @@ public class BintexReader {
 		}
 		
 		// max len = 255, maka buf pasti cukup
-		char[] _buf = this.buf;
+		char[] _buf = this.buf_char;
 		for (int i = 0; i < len; i++) {
-			_buf[i] = readCharTanpaNaikPos();
+			_buf[i] = readCharWithoutIncreasingPos();
 		}
 		pos_ += len + len;
 		
@@ -40,12 +63,12 @@ public class BintexReader {
 			return ""; //$NON-NLS-1$
 		}
 		
-		if (len > buf.length) {
-			this.buf = new char[len + 1024];
+		if (len > buf_char.length) {
+			this.buf_char = new char[len + 1024];
 		}
-		char[] _buf = this.buf;
+		char[] _buf = this.buf_char;
 		for (int i = 0; i < len; i++) {
-			_buf[i] = readCharTanpaNaikPos();
+			_buf[i] = readCharWithoutIncreasingPos();
 		}
 		pos_ += len + len;
 		
@@ -70,12 +93,12 @@ public class BintexReader {
 			len = readInt();
 		}
 		
-		if (len > buf.length) {
-			this.buf = new char[len + 1024];
+		if (len > buf_char.length) {
+			this.buf_char = new char[len + 1024];
 		}
 		
 		if (jenis == 0x01 || jenis == 0x11) {
-			char[] _buf = this.buf;
+			char[] _buf = this.buf_char;
 			for (int i = 0; i < len; i++) {
 				_buf[i] = (char) is_.read();
 			}
@@ -83,9 +106,9 @@ public class BintexReader {
 			
 			return new String(_buf, 0, len);
 		} else if (jenis == 0x02 || jenis == 0x12) {
-			char[] _buf = this.buf;
+			char[] _buf = this.buf_char;
 			for (int i = 0; i < len; i++) {
-				_buf[i] = readCharTanpaNaikPos();
+				_buf[i] = readCharWithoutIncreasingPos();
 			}
 			pos_ += len + len;
 			
@@ -107,7 +130,7 @@ public class BintexReader {
 		return res;
 	}
 	
-	private char readCharTanpaNaikPos() throws IOException {
+	private char readCharWithoutIncreasingPos() throws IOException {
 		return (char) ((is_.read() << 8) | (is_.read()));
 	}
 
@@ -155,7 +178,254 @@ public class BintexReader {
 
 		return total;
 	}
+
+	public int readValueInt() throws IOException {
+		int t = is_.read();
+		pos_++;
+		return _readValueInt(t);
+	}
 	
+	private int _readValueInt(int t) throws IOException {
+		switch (t) {
+		case 0x0e: // special value 0
+			return 0;
+		case 0x01:
+		case 0x02:
+		case 0x03:
+		case 0x04:
+		case 0x05:
+		case 0x06:
+		case 0x07: // immediate 1-7
+			return t;
+		case 0x0f: // special value -1
+			return -1;
+		case 0x10: 
+		case 0x11: { 
+			int a = is_.read();
+			pos_++;
+			return t == 0x11? ~a: a;
+		}
+		case 0x20: 
+		case 0x21: { 
+			int a = (is_.read() << 8) | (is_.read());
+			pos_ += 2;
+			return t == 0x21? ~a: a;
+		}
+		case 0x30: 
+		case 0x31: { 
+			int a = (is_.read() << 16) | (is_.read() << 8) | (is_.read());
+			pos_ += 3;
+			return t == 0x31? ~a: a;
+		}
+		case 0x40: 
+		case 0x41: { 
+			int a = (is_.read() << 24) | (is_.read() << 16) | (is_.read() << 8) | (is_.read());
+			pos_ += 4;
+			return t == 0x41? ~a: a;
+		}
+		default: {
+			throw new IOException(String.format("value is not int: type=%02x", t));
+		}
+		}
+	}
+	
+	public String readValueString() throws IOException {
+		int t = is_.read();
+		pos_++;
+		return _readValueString(t);
+	}
+	
+	private String _readValueString(int t) throws IOException {
+		switch (t) {
+		case 0x0c: // null
+			return null;
+		case 0x0d:
+			return "";
+		case 0x51:
+		case 0x52:
+		case 0x53:
+		case 0x54:
+		case 0x55:
+		case 0x56:
+		case 0x57:
+		case 0x58:
+		case 0x59:
+		case 0x5a:
+		case 0x5b:
+		case 0x5c:
+		case 0x5d:
+		case 0x5e:
+		case 0x5f: { // 8-bit string with len 1-15
+			int len = t & 0x0f;
+			return _read8BitString(len);
+		}
+		case 0x61:
+		case 0x62:
+		case 0x63:
+		case 0x64:
+		case 0x65:
+		case 0x66:
+		case 0x67:
+		case 0x68:
+		case 0x69:
+		case 0x6a:
+		case 0x6b:
+		case 0x6c:
+		case 0x6d:
+		case 0x6e:
+		case 0x6f: { // 16-bit string with len 1-15
+			int len = t & 0x0f;
+			return _read16BitString(len);
+		}
+		case 0x70: { // 8-bit string with len < 256
+			int len = is_.read();
+			pos_++;
+			return _read8BitString(len);
+		}
+		case 0x71: { // 16-bit string with len < 256
+			int len = is_.read();
+			pos_++;
+			return _read16BitString(len);
+		}
+		case 0x72: { // long 8-bit string
+			int len = readInt();
+			return _read8BitString(len);
+		}
+		case 0x73: { // long 16-bit string
+			int len = readInt();
+			return _read16BitString(len);
+		}
+		default: 
+			throw new IOException(String.format("value is not string: type=%02x", t));
+		}
+	}
+
+	private String _read8BitString(int len) throws IOException {
+		byte[] buf1 = len <= this.buf_byte.length? this.buf_byte: new byte[len];
+		is_.read(buf1, 0, len);
+		pos_ += len;
+		return new String(buf1, 0x00, 0, len);
+	}
+	
+	private String _read16BitString(int len) throws IOException {
+		int bytes = len << 1;
+		char[] buf2 = len <= this.buf_char.length? this.buf_char: new char[len];
+		for (int i = 0; i < len; i++) {
+			buf2[i] = readCharWithoutIncreasingPos();
+		}
+		pos_ += bytes;
+		return new String(buf2, 0, len);
+	}
+
+	public int[] readValueUint8Array() throws IOException {
+		int t = is_.read();
+		pos_++;
+		return _readValueUint8Array(t);
+	}
+	
+	private int[] _readValueUint8Array(int t) throws IOException {
+		int len;
+		if (t == 0xc0) { // len < 256
+			len = is_.read();
+			pos_++;
+		} else if (t == 0xc8) {
+			len = readInt();
+		} else {
+			throw new IOException(String.format("value is not uint8 array: type=%02x", t));
+		}
+		
+		byte[] buf1 = len <= this.buf_byte.length? this.buf_byte: new byte[len];
+		is_.read(buf1, 0, len);
+		pos_ += len;
+		
+		int[] res = new int[len];
+		for (int i = 0; i < len; i++) {
+			res[i] = buf1[i] & 0xff;
+		}
+		return res;
+	}
+
+	/** also returns correctly if the data is of type uint8 array */ 
+	public int[] readValueIntArray() throws IOException {
+		int t = is_.read();
+		pos_++;
+		return _readValueIntArray(t);
+	}
+	
+	private int[] _readValueIntArray(int t) throws IOException {
+		int len;
+		if (t == 0xc0) {
+			return _readValueUint8Array(t);
+		} else if (t == 0xc8) {
+			return _readValueUint8Array(t);
+		} else if (t == 0xc4) { // len < 256
+			len = is_.read();
+			pos_++;
+		} else if (t == 0xcc) {
+			len = readInt();
+		} else {
+			throw new IOException(String.format("value is not int array: type=%02x", t));
+		}
+		
+		int[] res = new int[len];
+		byte[] buf = new byte[4];
+		for (int i = 0; i < len; i++) {
+			is_.read(buf, 0, 4);
+			res[i] = ((buf[0] & 0xff) << 24) | ((buf[1] & 0xff) << 16) | ((buf[2] & 0xff) << 8) | (buf[3] & 0xff);
+		}
+		pos_ += len << 2;
+		
+		return res;
+	}
+	
+	public Map<String, Object> readValueSimpleMap() throws IOException {
+		int t = is_.read();
+		pos_++;
+		return _readValueSimpleMap(t);
+	}
+
+	private Map<String, Object> _readValueSimpleMap(int t) throws IOException {
+		if (t != 0x90) {
+			throw new IOException(String.format("value is not simple map: type=%02x", t));
+		}
+		
+		int size = is_.read();
+		pos_++;
+		
+		LinkedHashMap<String, Object> res = new LinkedHashMap<String, Object>();
+		
+		for (int i = 0; i < size; i++) {
+			int key_len = is_.read();
+			pos_++;
+			
+			String k = _read8BitString(key_len);
+			Object v = readValue();
+			
+			res.put(k, v);
+		}
+		
+		return res;
+	}
+
+	public Object readValue() throws IOException {
+		int t = is_.read();
+		pos_++;
+		
+		// ints
+		int type = SUPPORTED_TYPE_MAP[t];
+		if (type == 1) {
+			return _readValueInt(t);
+		} else if (type == 2) {
+			return _readValueString(t);
+		} else if (type == 3) {
+			return _readValueIntArray(t);
+		} else if (type == 4) {
+			return _readValueSimpleMap(t);
+		} else {
+			throw new IOException(String.format("value has unknown type: type=%02x", t));
+		}
+	}
+
 	public long skip(long n) throws IOException {
 		long res = is_.skip(n);
 		pos_ += (int) res;
--- BintexWriter/src/yuku/bintex/BintexWriter.java
+++ BintexWriter/src/yuku/bintex/BintexWriter.java
@@ -2,7 +2,53 @@ package yuku.bintex;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Map;
 
+/**
+ * VALUE types:
+ * A, B etc are unsigned byte
+ * 
+ * [value takes 1 byte]
+ * 01..07 = int 1..7
+ * 0c = string null
+ * 0d = string with zero length
+ * 0e = int 0
+ * 0f = int -1
+ * 
+ * [value takes 2 bytes]
+ * 10 A = int A
+ * 11 A = int ~A (negate of A, not minus A) 
+ * 
+ * [value takes 3 bytes]
+ * 20 A[2] = positive 16-bit int
+ * 21 A[2] = negate of 16-bit int
+ * 
+ * [value takes 4 bytes]
+ * 30 A[3] = positive 24-bit int
+ * 31 A[3] = negate of 24-bit int
+ * 
+ * [value takes 5 bytes]
+ * 40 A[4] = positive 32-bit int
+ * 41 A[4] = negate of 32-bit int
+ * 
+ * [variable length]
+ * 51..5f A[n] = 8-bit (all the characters are in range 0x0000-0x00ff) string with length n=1..15 
+ * 61..6f A[n*2] = 16-bit string with length n=1..15 
+ * 70 A B[A] = 8-bit string with length A
+ * 71 A B[A] = 16-bit string with length A
+ * 72 A[4] B[A] = 8-bit string with length A
+ * 73 A[4] B[A] = 16-bit string with length A
+ * 
+ * [maps]
+ * 90 A {B C[B], value}[A] = simple map with no more than 255 entries with 8-bit string keys
+ * (not-yet-implemented) value A {B C[B], value}[A] = same as above with total size of map specified
+ * 
+ * [arrays]
+ * c0 A B[A] = uint8 array with max 255 elements  
+ * c4 A B[A*4] = int array with max 255 elements  
+ * c8 A[4] B[A] = uint8 array
+ * cc A[4] B[A*4] = int array
+ */
 public class BintexWriter {
 	private final OutputStream os_;
 
@@ -91,7 +137,7 @@ public class BintexWriter {
 		os_.write((a & 0xff000000) >> 24);
 		os_.write((a & 0x00ff0000) >> 16);
 		os_.write((a & 0x0000ff00) >> 8);
-		os_.write((a & 0x000000ff) >> 0);
+		os_.write((a & 0x000000ff));
 		
 		pos += 4;
 	}
@@ -137,6 +183,234 @@ public class BintexWriter {
 		os_.write(buf, off, len);
 		
 		pos += len;
+	}
+	
+	public void writeValueInt(int a) throws IOException {
+		if (a == 0) {
+			os_.write(0x0e);
+			pos += 1;
+		} else if (a >= 1 && a <= 7) {
+			os_.write(a);
+			pos += 1;
+		} else if (a == -1) {
+			os_.write(0x0f);
+			pos += 1;
+		} else if (a > 0) {
+			if (a < 256) { 
+				os_.write(0x10);
+				os_.write(a);
+				pos += 2;
+			} else if (a < 65536) {
+				os_.write(0x20);
+				os_.write((a & 0xff00) >> 8);
+				os_.write(a & 0x00ff);
+				pos += 3;
+			} else if (a < 16777216) {
+				os_.write(0x30);
+				os_.write((a & 0xff0000) >> 16);
+				os_.write((a & 0x00ff00) >> 8);
+				os_.write(a & 0x0000ff);
+				pos += 4;
+			} else {
+				os_.write(0x40);
+				os_.write((a & 0xff000000) >> 24);
+				os_.write((a & 0x00ff0000) >> 16);
+				os_.write((a & 0x0000ff00) >> 8);
+				os_.write((a & 0x000000ff));
+				pos += 5;
+			}
+		} else {
+			a = ~a;
+			if (a < 256) { 
+				os_.write(0x11);
+				os_.write(a);
+				pos += 2;
+			} else if (a < 65536) {
+				os_.write(0x21);
+				os_.write((a & 0xff00) >> 8);
+				os_.write(a & 0x00ff);
+				pos += 3;
+			} else if (a < 16777216) {
+				os_.write(0x31);
+				os_.write((a & 0xff0000) >> 16);
+				os_.write((a & 0x00ff00) >> 8);
+				os_.write(a & 0x0000ff);
+				pos += 4;
+			} else {
+				os_.write(0x41);
+				os_.write((a & 0xff000000) >> 24);
+				os_.write((a & 0x00ff0000) >> 16);
+				os_.write((a & 0x0000ff00) >> 8);
+				os_.write((a & 0x000000ff));
+				pos += 5;
+			}
+		}
+	}
+	
+	public void writeValueString(String s) throws IOException {
+		// special case: null and length-0 strings
+		if (s == null) {
+			os_.write(0x0c);
+			pos += 1;
+		} else if (s.length() == 0) {
+			os_.write(0x0d);
+			pos += 1;
+		} else {
+			// check if all characters are 0x0000-0x00ff
+			boolean all8bits = true;
+			int len = s.length();
+			for (int i = 0; i < len; i++) {
+				char c = s.charAt(i);
+				if (c > 0xff) {
+					all8bits = false;
+					break;
+				}
+			}
+			
+			// write header and length
+			if (len < 16) {
+				if (all8bits) {
+					os_.write(0x50 | len);
+				} else {
+					os_.write(0x60 | len);
+				}
+				pos += 1;
+			} else if (len < 256) {
+				if (all8bits) {
+					os_.write(0x70);
+					os_.write(len);
+				} else {
+					os_.write(0x71);
+					os_.write(len);
+				}
+				pos += 2;
+			} else {
+				if (all8bits) {
+					os_.write(0x72);
+				} else {
+					os_.write(0x73);
+				}
+				pos += 1;
+				
+				writeInt(len);
+			}
+			
+			// write characters
+			if (all8bits) {
+				for (int i = 0; i < s.length(); i++) {
+					char c = s.charAt(i);
+					os_.write(c);
+				}
+				pos += len;
+			} else {
+				for (int i = 0; i < s.length(); i++) {
+					char c = s.charAt(i);
+					os_.write((c & 0xff00) >> 8);
+					os_.write(c & 0x00ff);
+				}
+				pos += len << 1;
+			}
+		}
+	}
+
+	public void writeValueUint8Array(int[] a) throws IOException {
+		int len = a.length;
+		if (len < 256) {
+			writeUint8(0xc0);
+			writeUint8(len);
+		} else {
+			writeUint8(0xc8);
+			writeInt(len);
+		}
+		
+		for (int e: a) {
+			if (e > 255) {
+				throw new RuntimeException("element is larger than 255");
+			}
+			os_.write(e);
+		}
+		pos += len;
+	}
+	
+	/** will use {@link #writeValueUint8Array(int[])} if possible */ 
+	public void writeValueIntArray(int[] a) throws IOException {
+		{ // check optimization 
+			boolean allUint8 = true;
+			for (int e: a) {
+				if (e > 255 || e < 0) {
+					allUint8 = false;
+					break;
+				}
+			}
+			if (allUint8) {
+				writeValueUint8Array(a);
+				return;
+			}
+		}
+
+		int len = a.length;
+		if (len < 256) {
+			writeUint8(0xc4);
+			writeUint8(len);
+		} else {
+			writeUint8(0xcc);
+			writeInt(len);
+		}
+		
+		for (int e: a) {
+			os_.write((e & 0xff000000) >> 24);
+			os_.write((e & 0x00ff0000) >> 16);
+			os_.write((e & 0x0000ff00) >> 8);
+			os_.write((e & 0x000000ff));
+		}
+		pos += len << 2;
+	}
+	
+	public void writeValueSimpleMap(Map<String, Object> map) throws IOException {
+		int size = map.size();
+		if (size > 255) {
+			throw new RuntimeException("entries of map max 255");
+		}
+		
+		for (Map.Entry<String, Object> e: map.entrySet()) {
+			Object v = e.getValue();
+			if (v instanceof String) {
+			} else if (v instanceof Number) {
+			} else if (v instanceof int[]) {
+			} else {
+				throw new RuntimeException("map entry values must be string or int or int array");
+			}
+			String k = e.getKey();
+			if (k == null || k.length() > 255) {
+				throw new RuntimeException("map entry keys must be string with length max 255");
+			}
+			for (int i = 0; i < k.length(); i++) {
+				char c = k.charAt(i);
+				if (c > 0xff) throw new RuntimeException("map entry keys must be string with 8-bit characters");
+			}
+		}
+		
+		os_.write(0x90);
+		os_.write(size);
+		pos += 2;
+		
+		for (Map.Entry<String, Object> e: map.entrySet()) {
+			String k = e.getKey();
+			os_.write(k.length());
+			pos += 1;
+			for (int i = 0; i < k.length(); i++) {
+				os_.write(k.charAt(i));
+			}
+			pos += k.length();
+			Object v = e.getValue();
+			if (v instanceof String) {
+				writeValueString((String) v);
+			} else if (v instanceof Number) {
+				writeValueInt(((Number) v).intValue());
+			} else if (v instanceof int[]) {
+				writeValueIntArray((int[]) v);
+			}
+		}
 	}
 	
 	public void close() throws IOException {
